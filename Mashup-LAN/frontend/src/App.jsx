59fd86443c0f9ef27a304c336b5bd2bba9f77abf
Stanescu Victor <stanescuvictor12@gmail.com>
2025-09-19 22:16:50 +0200

    created 2 pages / added Tailwind

--- Mashup-LAN/frontend/src/App.jsx
+++ Mashup-LAN/frontend/src/App.jsx
@@ -1,16 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Admin from './pages/Admin';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>hello world</div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/admin" element={<Admin />} />
+        </Routes>
+      </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
